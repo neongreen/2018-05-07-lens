8eadacbec94899851adf9d5fba96f6f1fc118127
Artyom Kazak <yom@artyom.me>
2018-05-07 21:30:25 +0200

    field -> lens

--- lens.hs
+++ lens.hs
@@ -29,15 +29,15 @@ artyom = Person "Artyom" 22 (Address "Berlin" "Germany") ()
 
 -- First-class fields
 
-type Field r r' a a' =
+type Lens r r' a a' =
   forall f. Functor f => (a -> f a') -> r -> f r'
 
-type Field' r a = Field r r a a
+type Lens' r a = Lens r r a a
   -- forall f. Functor f => (a -> f a) -> r -> f r
 
 modify
   :: forall a r a' r'.
-     Field r r' a a' -> (a -> a') -> r -> r'
+     Lens r r' a a' -> (a -> a') -> r -> r'
 modify ra f r = runIdentity $ r_func r
 
   where
@@ -49,7 +49,7 @@ modify ra f r = runIdentity $ r_func r
     r_func :: r -> Identity r'
     r_func = ra f'
 
-get :: forall a r a' r'. Field r r' a a' -> r -> a
+get :: forall a r a' r'. Lens r r' a a' -> r -> a
 get ra r = getConst $ r_func r
   where
     f' :: a -> Const a a'
@@ -58,29 +58,29 @@ get ra r = getConst $ r_func r
     r_func :: r -> Const a r'
     r_func = ra f'
 
--- Definitions of 'Field's for all fields
+-- Definitions of 'Lens's for all fields
 
-namefield :: Field' (Person e) String
+namefield :: Lens' (Person e) String
 namefield = \f person ->
       (\l -> person {name = l}) <$> f (name person)
 
-addressfield :: Field' (Person e) Address
+addressfield :: Lens' (Person e) Address
 addressfield = \f person ->
       (\l -> person {address = l}) <$> f (address person)
 
-cityfield :: Field' Address String
+cityfield :: Lens' Address String
 cityfield = \f address ->
       (\l -> address {city = l}) <$> f (city address)
 
 -- Helpers
   
-(+=) :: Num a => Field' r a -> a -> (r -> r)
+(+=) :: Num a => Lens' r a -> a -> (r -> r)
 (+=) field n = (modify field) (+n)
 
-(%=) :: Field r r' a a' -> (a -> a') -> (r -> r')
+(%=) :: Lens r r' a a' -> (a -> a') -> (r -> r')
 (%=) = modify
 
-(.=) :: Field r r' a a' -> a' -> (r -> r')
+(.=) :: Lens r r' a a' -> a' -> (r -> r')
 (.=) field = modify field . const
 
 infix 5 +=, %=, .=
@@ -92,6 +92,6 @@ oldArtyom = artyom
   & addressfield.cityfield .= "Heidelberg"
 
   
-extrafield :: Field (Person e) (Person e') e e'
+extrafield :: Lens (Person e) (Person e') e e'
 extrafield = \f person -> 
       (\l -> person {extra = l}) <$> f (extra person)
